e3777967392e34f3056ce4f153e6334834e0d0a8
Sevada Gh <sevada.ghazaryan@gmail.com>
2019-08-25 21:11:09 +0400

    added binary to file

--- binary_upload.php
+++ binary_upload.php
@@ -1,4 +1,5 @@
 <?php
+
 $configs = include('config.php');
 require_once 'ConverterClass.php';
 require_once 'UploaderClass.php';
@@ -7,25 +8,31 @@ $docFile = new ConverterClass();
 $uploader = new UploaderClass();
 
 $uploader->setDir($configs['uploaddir']);
-$uploader->allowAllFormats();
-//$uploader->setExtensions(array('jpg','jpeg','png','gif', 'doc', 'docx', 'xls', 'xlsx', 'ppt', 'pptx', 'pdf', 'txt'));  //allowed extensions list
-$uploader->setMaxSize(15);                         //set max file size to be allowed in MB//
+//$uploader->allowAllFormats();
+$uploader->setExtensions(array('txt'));                    //allowed extensions list
+$uploader->setMaxSize(90);                         //set max file size to be allowed in MB//
 
-if($uploader->uploadFile('file')){               //txtFile is the filebrowse element name //
+$fileExtension = $_POST['ext'];
+if(empty($fileExtension))
+{
+    $fileExtension = '.data';
+}
+
+if($uploader->uploadFile('binaryfile')){         //txtFile is the filebrowse element name //
     $document  =   $uploader->getUploadName();             //get uploaded file name, renames on upload//
 
-    $docFile->setConvertedDir('storage/converted/');
+    $docFile->setConvertedDir('storage/');
     $docFile->setFileName("storage/$document");
     $docFile->space = false;
     $fileName = $docFile->getFileName();
 
     //var_dump($fileName);
-    $docFile->FileToBinary($fileName);
-    //$docFile->BinaryToBase64("bindata.txt");
-    //$docFile->Base64ToFile('storage/converted/final_base64.txt', 'storage/converted/gen_'.$document);
+    //$docFile->FileToBinary($fileName);
+    $docFile->BinaryToBase64(basename($fileName));
+    $docFile->Base64ToFile('storage/final_base64.txt', 'storage/converted/generatedFile'.$fileExtension);
 
     echo json_encode(['input_file'=>"/doc2bin/storage/".$document, 'only_name'=>$document, 'binary'=>"/doc2bin/storage/converted/bindata.txt", 'tmp_b64'=>"/doc2bin/storage/converted/temp.txt", 'back_b64'=>"/doc2bin/storage/converted/final_base64.txt",
-        'final_doc'=>"/doc2bin/storage/converted/gen_".$document]);
+        'final_doc'=>"/doc2bin/storage/converted/generatedFile".$fileExtension]);
 
 
 }else{//upload failed
